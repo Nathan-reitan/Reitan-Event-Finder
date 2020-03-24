c28c46035429dd890f3f4ad94cda8794dc69ebeb
Nathan Reitan <nathanreitan@gmail.com>
2020-03-24 12:14:17 -0700

    map setup, navigator setup

---
+++ components/googleMaps.js
@@ -0,0 +1,10 @@
+function initMap(){
+  var yourLocation = {lat: lat, lng: long,};
+  var map = new google.maps.Map(
+    document.getElementById("map"),{
+      zoom: 10,
+      center: yourLocation,
+    }
+  );
+  var marker = new google.maps.Marker({ position: yourLocation, map: map })
+}
