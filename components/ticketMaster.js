e1b11f3f4e9fcb2dbce61341fb087bd172475f0e
Nathan Reitan <nathanreitan@gmail.com>
2020-03-24 17:43:33 -0700

    table populating based off of lat and long

---
+++ components/ticketMaster.js
@@ -0,0 +1,37 @@
+var tbody = document.getElementById("table");
+
+
+class Ticketmaster{
+  constructor(latlong){
+    this.latlong=latlong;
+    this.getEvents=this.getEvents.bind(this);
+  }
+  getEvents(){
+    $.ajax({
+    type: "GET",
+      url: "https://app.ticketmaster.com/discovery/v2/events.json?size=50&apikey=uPhG93gkA9zk83eAg0Q2AvsIbEOSrASU&radius=30&latlong="+this.latlong,
+    dataType: "json",
+    success: function (json) {
+      console.log(json);
+      for (var i = 0; i < json._embedded.events.length; i++){
+        var row = document.createElement("tr");
+        var eventName = document.createElement("td");
+        eventName.textContent = json._embedded.events[i].name;
+        var eventVenue = document.createElement("td");
+        eventVenue.textContent = json._embedded.events[i]._embedded.venues[0].name;
+        var eventCity = document.createElement("td");
+        eventCity.textContent = json._embedded.events[i]._embedded.venues[0].city.name
+        // console.log(json._embedded.events[i].name);
+        // console.log(json._embedded.events[i]._embedded.venues[0].name);
+        // console.log(json._embedded.events[i]._embedded.venues[0].city.name)
+        row.appendChild(eventName);
+        row.appendChild(eventVenue);
+        row.appendChild(eventCity);
+        tbody.appendChild(row);
+      }
+    },
+      error: function (xhr, status, err) {
+      }
+    })
+  }
+}
